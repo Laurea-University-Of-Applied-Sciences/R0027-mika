38c06f574cd10203395d8fcc682eaa4c84d2b8f8
Mika Stenberg <mika.stenberg@laurea.fi>
2021-03-02 13:24:55 +0200

    Delete ErikoismerkkienTulostus.java

--- ErikoismerkkienTulostus.java
+++
@@ -1,7 +0,0 @@
-package Workshop_B_Variables;
-
-public class ErikoismerkkienTulostus {
-	public static void main(String[] args) {	
-		System.out.print("IDE: \"Eclipse\"\nWorkspace: \"H:\\eclipse_workspace\"");
-	}
-}
